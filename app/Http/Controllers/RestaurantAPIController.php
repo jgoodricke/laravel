8774856fddeae3e29636906743acc8f64b38568f
James <jggoodricke@gmail.com>
2018-05-28 01:15:42 +1000

    re-did merge conflict correction

--- app/Http/Controllers/RestaurantAPIController.php
+++ app/Http/Controllers/RestaurantAPIController.php
@@ -1,14 +1,9 @@
 <?php
-<<<<<<< HEAD
-namespace App\Http\Controllers;
-use Illuminate\Http\Request;
-=======
 
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
->>>>>>> 2af26f48eea78e604ffc7655a98264e9710cb7cc
 use App\Restaurant;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\View;
@@ -18,75 +13,6 @@ use Input;
 use Session;
 use Redirect;
 
-<<<<<<< HEAD
-use App\Http\Requests\StoreRestaurant;
-
-class RestaurantAPIController extends Controller
-{
-  public function index()
-  {
-    return Restaurant::all();
-  }
-  public function create()
-  {
-  //
-  }
-
-  public function store(StoreRestaurant $request)
-  {
-    $Restaurant = Restaurant::create($request->all());
-    return response()->json($Restaurant, 201);
-  }
-
-  public function show(Request $request)
-  {
-    $rules = array(
-      'id' => 'required|integer|min:0',
-    );
-    $validator = Validator::make(Input::all(), $rules);
-    if ($validator->fails()) {
-      return response()->json(['errors'=>$validator->errors()]);
-    } else {
-      $Restaurant = Restaurant::find($request['id']);
-      return response()->json($Restaurant, 201);
-    }
-  }
-
-  public function edit($id)
-  {
-    //
-  }
-
-  public function update(StoreRestaurant $request)
-  {
-    $rules = array(
-      'id' => 'required|integer|min:0',
-    );
-    $validator = Validator::make(Input::all(), $rules);
-    if ($validator->fails()) {
-      return response()->json(['errors'=>$validator->errors()]);
-    } else {
-      $Restaurant = Restaurant::find($request['id']);
-      $Restaurant->update($request->all());
-      return response()->json($Restaurant, 200);
-    }
-  }
-
-  public function destroy(Request $request)
-  {
-    $rules = array(
-      'id' => 'required|integer|min:0',
-    );
-    $validator = Validator::make(Input::all(), $rules);
-    if ($validator->fails()) {
-      return response()->json(['errors'=>$validator->errors()]);
-    } else {
-      $Restaurant = Restaurant::find($request['id']);
-      $Restaurant->delete();
-      return response()->json(null, 204);
-    }
-  }
-=======
 class RestaurantAPIController extends Controller
 {
     /**
@@ -207,5 +133,4 @@ class RestaurantAPIController extends Controller
         $restaurant->delete();
         return response()->json(null, 204);
     }
->>>>>>> 2af26f48eea78e604ffc7655a98264e9710cb7cc
 }
